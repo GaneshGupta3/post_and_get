ccefe736c393afa9109c2a2c10e580d81a8e1c4a
GaneshGupta3 <ganesh310104@gmail.com>
2024-09-17 06:36:18 +0000

    completed till the social media project

--- learning_react/1-todo-app-version-two/src/App.jsx
+++ learning_react/1-todo-app-version-two/src/App.jsx
@@ -1,107 +1,25 @@
 import AppName from "./components/AppName";
 import AddTodo from "./components/AddTodo";
 import TodoItems from "./components/TodoItems";
-import { useState } from "react";
+import { useReducer } from "react";
 
 import "./App.css";
-import { TodoItemsContext } from "./store/todo_items_store";
+import TodoItemsContextProvider, { TodoItemsContext } from "./store/todo_items_store";
+
+
 
 function App() {
-  const [todoItems, setTodoItems] = useState([
-    {
-      todoName: "Buy Milk",
-      todoDate: "4/10/2023",
-    },
-    {
-      todoName: "Go to College",
-      todoDate: "4/10/2023",
-    },
-  ]);
   
 
-
-  function formatDate(date) {
-    let newDate = new Date(date);
-    let day = newDate.getDate();
-    let month = newDate.getMonth() + 1;
-    let year = newDate.getFullYear();
-    return day + "/" + month + "/" + year;
-  }
-
-  /*function addTodo(todoDate , todoName){
-    console.log(todoName , todoDate);
-    if(todoDate==null || todoName==null){
-      return ;
-    }
-    else{
-      let newData = {
-        todoName : todoName,
-        todoDate : formatDate(todoDate)
-      }
-      setTodoItems([...todoItems , newData]);
-    }
-  }*/
-
-  function addTodo(todoName, todoDate) {
-    console.log(todoName, todoDate);
-    if (todoDate == null || todoName == null) {
-      return null;
-    } else {
-      setTodoItems((currVal) => {
-        return [
-          ...currVal,
-          {
-            todoName: todoName,
-            todoDate: formatDate(todoDate),
-          },
-        ]; //we have to return it otherwise it will give error and using this because of async nature of react
-      });
-    }
-  }
-
-  function deleteTodo(todoName, todoDate) {
-    let deleteData = {
-      todoName: todoName,
-      todoDate: todoDate,
-    };
-    console.log(deleteData);
-    const deletedItem = todoItems.find(
-      (item) => item.todoDate === todoDate && item.todoName === todoName
-    );
-    console.log(deletedItem);
-    if(deletedItem===null){
-      return null;
-    }
-
-    alert(`deleted the task "${deletedItem.todoName}" that was to be completed on the date of "${deletedItem.todoDate}"`)
-
-    setTodoItems(()=>
-      todoItems.filter(
-        (item) => item.todoDate !== todoDate || item.todoName != todoName
-      )
-    );
-  }
-
-  function confirmDeleteTodo(todoName,todoDate){
-    const confirmation = confirm(`do you want to delete todo with name:${todoName} and on date : ${todoDate}`);
-    console.log(confirmation);
-    if(confirmation) {
-      deleteTodo(todoName,todoDate);
-    }
-  }
-
   return (
-    <TodoItemsContext.Provider
-      value={{ todoItems, addTodo, confirmDeleteTodo }}
-    >
-      
+    <TodoItemsContextProvider>
       {/*we pass the state elements to the context provider so that we can repaint on the change of it*/}
       <center className="todo-container">
         <AppName />
-        <AddTodo onAddClick={addTodo} />
+        <AddTodo />
         <TodoItems></TodoItems>
       </center>
-    </TodoItemsContext.Provider>
+    </ TodoItemsContextProvider>
   );
 }
 
--- learning_react/1-todo-app-version-two/src/store/todo_items_store.jsx
+++ learning_react/1-todo-app-version-two/src/store/todo_items_store.jsx
@@ -1,3 +1,118 @@
 import { createContext } from "react";
+import { useReducer } from "react";
 
-export const TodoItemsContext = createContext({ todoItems : [], addTodo: ()=>{}, confirmDeleteTodo: ()=>{} });{/*declared value here because we want auto suggestion otherwise we can give the value*/}
+export const TodoItemsContext = createContext({
+  todoItems: [],
+  addTodo: () => {},
+  confirmDeleteTodo: () => {},
+});
+{
+  /*declared value here because we want auto suggestion otherwise we can give the value*/
+}
+
+const todoItemReducer = (currTodoItems, action) => {
+  {
+    /* currTodoItems is the current state of the items we defined the action type and payload while calling the todoItemReducer using dispatchTodoItems */
+  }
+
+  let newTodoItems = currTodoItems;
+
+  if (action.type === "ADDTODO") {
+    if (action.payload.todoDate == null || action.payload.todoName == null) {
+      return null;
+    } else {
+      newTodoItems = [
+        ...currTodoItems,
+        {
+          todoName: action.payload.todoName,
+          todoDate: action.payload.todoDate,
+        },
+      ]; //we have to return it otherwise it will give error and using this because of async nature of react
+    }
+  } else if (action.type === "DELTODO") {
+    let deleteData = {
+      todoName: action.payload.todoName,
+      todoDate: action.payload.todoDate,
+    };
+
+    const deletedItem = newTodoItems.find(
+      (item) =>
+        item.todoDate === deleteData.todoDate &&
+        item.todoName === deleteData.todoName
+    );
+
+    if (deletedItem === null) {
+      return null;
+    }
+
+    newTodoItems = newTodoItems.filter(
+      (item) =>
+        item.todoDate !== deletedItem.todoDate ||
+        item.todoName != deletedItem.todoName
+    );
+  }
+  return newTodoItems;
+};
+
+const TodoItemsContextProvider = ({ children }) => {
+  const [todoItems, dispatchTodoItems] = useReducer(todoItemReducer, [
+    {
+      todoName: "Buy Milk",
+      todoDate: "12/12/2022",
+    },
+    {
+      todoName: "Buy Ghee",
+      todoDate: "12/12/2022",
+    },
+  ]);
+
+  function formatDate(date) {
+    let newDate = new Date(date);
+    let day = newDate.getDate();
+    let month = newDate.getMonth() + 1;
+    let year = newDate.getFullYear();
+    return day + "/" + month + "/" + year;
+  }
+
+  function addTodo(todoName, todoDate) {
+    todoDate = formatDate(todoDate);
+    console.log(todoName + todoDate);
+    const newTodoAction = {
+      type: "ADDTODO",
+      payload: {
+        todoName,
+        todoDate,
+      },
+    };
+    dispatchTodoItems(newTodoAction);
+  }
+
+  function deleteTodo(todoName, todoDate) {
+    const newTodoDeleteAction = {
+      type: "DELTODO",
+      payload: {
+        todoName,
+        todoDate,
+      },
+    };
+    dispatchTodoItems(newTodoDeleteAction);
+  }
+
+  function confirmDeleteTodo(todoName, todoDate) {
+    const confirmation = confirm(
+      `do you want to delete todo with name:${todoName} and on date : ${todoDate}`
+    );
+    if (confirmation) {
+      deleteTodo(todoName, todoDate);
+    }
+  }
+  return (
+    <TodoItemsContext.Provider
+      value={{ todoItems, addTodo, confirmDeleteTodo }}
+    >
+      {children}
+    </TodoItemsContext.Provider>
+  );
+};
+
+export default TodoItemsContextProvider;
